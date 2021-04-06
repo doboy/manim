c1068aaff9ce74b315150577e180eb3d7a7d5ce1
Zack Do <doboy0@gmail.com>
2021-04-05 22:45:55 -0400

    modify dfs

--- scene_dfs.py
+++ scene_dfs.py
@@ -1,12 +1,18 @@
+# -r 1900,600
+
 from collections import defaultdict, deque
 
 from mdeque import Mdeque
+from mval import Mval
+from mnode import Mnode, Mhiddennode
+import numpy as np
 from manim import *
 import networkx as nx
+from colors import *
 
-TEXT_SCALE = .3
-SQUARE_SIDE_LENGTH = 1
-DISCOVERED_COLOR = PURPLE
+UNVISITED_COLOR = RED_C
+VISITED_COLOR = GREEN_SCREEN
+CURRENT_NODE_COLOR = YELLOW_C
 
 class BFS(Scene):
   def construct(self):
@@ -31,28 +37,43 @@ class BFS(Scene):
     edges = [
       ("A", "B"),
       ("A", "C"),
-      ("C", "D"),
+      # ("C", "D"),
     ]
 
     max_queue_size, discovered_length = self.dfs("A", edges, animate=False)
     self.visited_mdeque = Mdeque(discovered_length).to_edge(DL)
-    self.current_node_mdeque = Mdeque(1).to_edge(DR)
-    self.queue_mdeque = Mdeque(max_queue_size, direction=UP).next_to(self.current_node_mdeque, direction=UP)
+    self.current_placeholder = Mval().to_edge(DR)
+    self.queue_mdeque = Mdeque(max_queue_size, direction=UP).next_to(self.current_placeholder, direction=UP)
     self.play(
       Create(self.visited_mdeque),
       Create(self.queue_mdeque),
-      Create(self.current_node_mdeque),
+      Create(self.current_placeholder),
+    )
+
+    visited_order_text = Text("Visited Order").next_to(self.visited_mdeque, direction=UP).scale_in_place(.5)
+    queue_text = Text("Queue").next_to(self.queue_mdeque, direction=UP).scale_in_place(.5)
+    current_node_text = Text("Current Node").next_to(self.current_placeholder, direction=DOWN).scale_in_place(.5)
+    self.play(
+      Create(visited_order_text),
+      Create(queue_text),
+      Create(current_node_text),
     )
 
     labels = {}
     for vertex in vertices:
-      labels[vertex] = Text(vertex).set_color(WHITE).scale_in_place(.3)
+      labels[vertex] = Text(vertex).scale_in_place(.8)
 
     self.graph = Graph(
       vertices,
       edges,
       layout="circular",
-      vertex_config={"fill_opacity": 0, "color": WHITE, "stroke_width": 5, "radius": .4},
+      vertex_type=Mhiddennode,
+      vertex_config={
+        "fill_opacity": 0,
+        "color": UNVISITED_COLOR,
+        "stroke_width": 5,
+        "radius": .4
+      },
       labels=labels,
     )
 
@@ -70,42 +91,65 @@ class BFS(Scene):
     queue = deque([start_node])
 
     if animate:
-      m_start_node = self.graph.vertices[start_node].copy()
       self.play(
-        *self.queue_mdeque.append(m_start_node)[0],
+        *self.queue_mdeque.append(self.graph.vertices[start_node].copy())[0],
       )
 
     max_queue_size = 0
+    current = None
+    m_current = None
     while len(queue) > 0:
       current = queue.popleft()
       if animate:
-        if len(self.current_node_mdeque):
-          _, m_element = self.current_node_mdeque.pop()
-          visit_animations = self.visited_mdeque.append(m_element)[0]
-          self.play(
-            *visit_animations,
-          )
-
         shift_animations, m_current = self.queue_mdeque.popleft(animate_fadeout=False)
-        move_animations = self.current_node_mdeque.append(m_current)[0]
-        self.play(
-          *move_animations,
-          *shift_animations
-        )
+        m_current_copy = m_current.copy().move_to(self.current_placeholder).set_circle_color(CURRENT_NODE_COLOR)
 
+        self.play(
+          ApplyMethod(self.graph.vertices[current].set_circle_color, CURRENT_NODE_COLOR),
+          Transform(m_current, m_current_copy),
+          *shift_animations,
+        )
 
       for neighbor in graph[current]:
         if neighbor not in discovered:
           discovered.add(neighbor)
           queue.append(neighbor)
           if animate:
+            start = self.graph.vertices[current].get_center()
+            end = self.graph.vertices[neighbor].get_center()
+            direction = (end - start) / np.linalg.norm(end - start)
+
+            arrow = Arrow(
+              self.graph.vertices[current].get_center() + direction * .2,
+              self.graph.vertices[neighbor].get_center() - direction * .2,
+            ).set_color(CURRENT_NODE_COLOR)
+
             self.play(
-              ApplyMethod(self.graph.vertices[neighbor].set_color, DISCOVERED_COLOR),
+              GrowArrow(arrow)
             )
-            m_neighbor = self.graph.vertices[neighbor].copy()
+
+            self.graph.vertices[neighbor].hidden.move_to(self.graph.vertices[neighbor].label)
             self.play(
-              *self.queue_mdeque.append(m_neighbor)[0],
+              Transform(self.graph.vertices[neighbor].label, self.graph.vertices[neighbor].hidden),
+              FadeOut(arrow),
+            )
+
+            self.play(
+              *self.queue_mdeque.append(
+                self.graph.vertices[neighbor].copy()
+              )[0],
             )
           max_queue_size = max(max_queue_size, len(queue))
+
+      if animate:
+        self.visited_mdeque.append(m_current.copy().set_circle_color(VISITED_COLOR), move=True)
+        self.play(
+          ApplyMethod(self.graph.vertices[current].set_circle_color, VISITED_COLOR),
+          Transform(m_current, self.visited_mdeque.m_elements[-1]),
+        )
+
+
+    if animate:
+      self.wait(5)
 
     return max_queue_size, len(discovered)
